aad62d77e1860e63c82035e7febb7e32042428ed
Lukasz Malucha <lucasmalucha@gmail.com>
2019-04-02 19:41:41 +0100

    heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 ## App Utilities
 import os
-import env
+# import env
 import geocoder
 from db import db
 from flask_pymongo import PyMongo
@@ -104,8 +104,8 @@ if __name__ == '__main__':
         def create_tables():
             db.create_all()
 
-    app.run(debug=True)
+    # app.run(debug=True)
 
 ## Heroku
-# port = int(os.environ.get('PORT', 5000))
-# app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
